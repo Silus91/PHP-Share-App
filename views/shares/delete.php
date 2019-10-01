e20e9d9400d3f64521134019edcec3417b380ca0
Dawid Dyczek <wizard91@o2.pl>
2019-10-01 22:59:34 +0100

    share delete in progress

---
+++ views/shares/delete.php
@@ -0,0 +1,7 @@
+<!-- <?php
+if(isset($_POST['delete_btn'])){
+    $id= $_POST['delete_id'];
+
+    $query = "DELETE FROM shared WHERE id='$id";
+    $query_run = mysqli_query(,$query)
+} ?>-->
--- views/shares/index.php
+++ views/shares/index.php
@@ -7,7 +7,9 @@
       <h3 class="h3 mb-3 font-weight-normal text-center"><?php echo $item['title']; ?></h3>
       <p class="text-center"><?php echo $item['body']; ?></p>
       <a class="btn btn-lg btn-primary mx-auto" href="<?php echo $item['link']; ?>" target="_blank">Go to Link</a>
-      <a class="btn btn-lg btn-danger mx-auto" href="<?php echo ROOT_PATH?>shares $item['id']; ?>" >Delete Post</a>
+      <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+        <input type="submit" name="delete_id" value="<?php echo $item['id']; ?>">
+      </form>
     </div>
   <?php endforeach; ?>
 </div>
