5e475b74d5699962742d72355a7bb7e81133b0c4
Dawid Dyczek <wizard91@o2.pl>
2019-10-07 17:28:07 +0100

    Todo app added

---
+++ views/todos/index.php
@@ -0,0 +1,14 @@
+<div>
+  <?php if(isset($_SESSION['is_logged_in'])) : ?>
+    <a  style="margin-top:5px" class="btn btn-lg btn-primary mx-auto btn-block" href="<?php echo ROOT_PATH?>todos/add"> Add Todo</a>
+  <?php endif; ?>
+  <?php foreach($viewmodel as $item) : ?>
+    <div class="container bg-light border border-warning rounded-lg" style="margin:5px">
+      <h3 class="h3 mb-3 font-weight-normal text-center"><?php echo $item['title']; ?></h3>
+      <p class="text-center"><?php echo $item['body']; ?></p>
+      <form action="<?php echo ROOT_PATH."todos/delete"; ?>" method="post">
+        <input class="btn btn-lg btn-success mx-auto" type="submit" name="delete_id" value="Done" />
+      </form>
+    </div>
+  <?php endforeach; ?>
+</div>
