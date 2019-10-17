3a66bdf1ef5c152c2aa6329d8960630eb2569342
Dawid Dyczek <wizard91@o2.pl>
2019-10-17 23:12:21 +0100

    typo fix

--- views/home/index.php
+++ views/home/index.php
@@ -3,9 +3,9 @@
   <p>You are on Share App, you are more than Welocome to register and start enjoying our Share app.</p>
   <br />
   <?php if(!isset($_SESSION['is_logged_in'])) : ?>
-  <p>Can`t see content?? Register to see all GOOD Stuff here!!</p>
+  <p>Can`t see the content?? Register to see all GOOD Stuff here!!</p>
   <button class="btn btn-primary">
-    <span class="spinner-border spinner-border-sm">Loading..</span>
+    <span class="spinner-border spinner-border-sm">Loading...</span>
   </button>
   <?php endif ?>
 </div>
